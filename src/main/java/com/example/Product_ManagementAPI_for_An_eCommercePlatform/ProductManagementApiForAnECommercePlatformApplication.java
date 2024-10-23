2d99d2a67147d0dc154a5544673de4c12d663b22
paultomal <tamal.paul.42123@gmail.com>
2024-10-23 23:10:20 +0600

    Initial Commit

---
+++ src/main/java/com/example/Product_ManagementAPI_for_An_eCommercePlatform/ProductManagementApiForAnECommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.Product_ManagementAPI_for_An_eCommercePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementApiForAnECommercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementApiForAnECommercePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Product_ManagementAPI_for_An_eCommercePlatform/ProductManagementApiForAnECommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Product_ManagementAPI_for_An_eCommercePlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApiForAnECommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
